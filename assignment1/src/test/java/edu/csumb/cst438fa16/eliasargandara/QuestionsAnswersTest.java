640e2e3358a6651ddfcb8e55864f8eefa1a475ee
Eliasar Gandara <elgandara@csumb.edu>
2016-09-06 23:53:50 -0700

    Added QuestionsAnswers class and implementations. Added QuestionsAnswersTest class
    Summary: Add partially completed test file
    
    Test Plan: Test the QuestionsAnswers put method
    
    Reviewers: benfallas
    
    Subscribers: NathanielDavidson, sorenlassen-csumb
    
    Differential Revision: https://reviews.facebook.net/D63717

--- assignment1/src/test/java/edu/csumb/cst438fa16/eliasargandara/QuestionsAnswersTest.java
+++ assignment1/src/test/java/edu/csumb/cst438fa16/eliasargandara/QuestionsAnswersTest.java
@@ -2,12 +2,25 @@ package edu.csumb.cst438fa16.eliasargandara;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class QuestionsAnswersTest {
+	
+	QuestionsAnswers questionsAnswers;
+	
+	@Before
+	public void initialization() {
+		questionsAnswers = new QuestionsAnswers();
+	}
 
 	@Test
-	public void test() {
+	public void put_whenEmpty() {
+		assertEquals(questionsAnswers.getRandomQuestion(), null);
+	}
+	
+	@Test
+	public void put_whenInserting() {
 		
 	}
 
